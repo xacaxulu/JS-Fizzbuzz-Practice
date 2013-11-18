ebafc015184f8a8a35b58b7794c8898754664b0f
lobsterhands <denmanlp@gmail.com>
2013-11-18 01:27:14 -0600

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -8,3 +8,10 @@
 
 // If you don't have an environment set up yet thats okay. you can use http://repl.it/languages/JavaScript
 // Then cut and paste the code here.
+
+// I did this without looking anything up based on the little bit that I've learned so far. Feels good. -lh
+var count = 0;
+var plus1 = +1;
+while (count <= 99) {
+    count = count + plus1;
+    console.log(count)}
