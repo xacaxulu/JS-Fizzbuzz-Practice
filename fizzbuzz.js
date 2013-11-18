7d2612c34560f1fa45cc3b5f84823dd9ba824450
James Denman <jdenman@epublishing.com>
2013-11-18 00:24:44 -0600

    first commit

---
+++ fizzbuzz.js
@@ -0,0 +1,10 @@
+// Write a program that prints the numbers from 1 to 100. 
+// But for multiples of three print "Fizz" instead of the number 
+// and for the multiples of five print "Buzz". 
+// For numbers which are multiples of both three and five print "FizzBuzz".
+
+// For Starters Lobsterhands, just write a program that prints the numbers from 1 to 100
+// Do it in this file below these comments:
+
+// If you don't have an environment set up yet thats okay. you can use http://repl.it/languages/JavaScript
+// Then cut and paste the code here.
