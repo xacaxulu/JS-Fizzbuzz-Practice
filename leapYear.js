1f533b3a41c95218dfd453032267ee5a51ddf9b1
Lake Denman <lake@lakedenman.com>
2013-11-19 10:30:31 -0500

    add leapYear exercise

---
+++ leapYear.js
@@ -0,0 +1,13 @@
+//  Leap Years. 
+
+//  Leap years are years divisible by four (like 1984 and 2004). 
+//  Years divisible by 100 are not leap years (such as 1800 and 1900), 
+//  unless they are divisible by 400 (like 1600 and 2000).
+
+//  Write a program which has variables for startYear and endYear.
+
+//  Then console.log all of the leap years between the starting year and ending 
+//  year (and include starting year/ending year if they are also leap years).
+
+//  Yes, it's all pretty confusing, but not as confusing as having July in the middle of the 
+//  winter, which is what would eventually happen.
