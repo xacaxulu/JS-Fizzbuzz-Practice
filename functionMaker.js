16c941d38e5fb4c6d2aaa6019aa1eebb6cc4c9c0
lobsterhands <denmanlp@gmail.com>
2013-11-20 22:59:48 -0600

    Update functionMaker.js

--- functionMaker.js
+++ functionMaker.js
@@ -1,1 +1,10 @@
 //make a function that takes in any string and returns the string reversed
+
+// I had to read a lot and play a lot to figure out how to make this work. -lh
+function reverse(word) {
+    var split = word.split("");
+    var rev = split.reverse("");
+    rev = rev.join("");
+    console.log(rev);
+}
+reverse("example");
