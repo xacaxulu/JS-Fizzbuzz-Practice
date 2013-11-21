82508e876519a3ec7aa54efed84375d642c5ea8f
Lake Denman <lake@lakedenman.com>
2013-11-21 15:31:31 -0500

    Add 99bottles stub

---
+++ 99bottles.js
@@ -0,0 +1,30 @@
+// 99 bottles
+// Since you know how to use for loops now, 
+// try to make a function that counts down from 99 to 0 and signs 99 bottles of beer on the wall.
+//
+// Fill in the details inside the loop to print the following:
+//     ...
+//     ...
+//
+//     3 bottles of beer on the wall
+//     3 bottles of beer
+//     take one down, pass it around
+//     2 bottles of beer on the wall
+//
+//     2 bottles of beer on the wall
+//     2 bottles of beer
+//     take one down, pass it around
+//     1 bottle of beer on the wall
+//
+//     1 bottle of beer on the wall
+//     1 bottle of beer
+//     take one down, pass it around
+//     0 bottles of beer on the wall
+//
+// You will want to watch out for bottle vs bottles.
+
+function sing() {
+  console.log("Do something interesting");
+}
+
+sing();
